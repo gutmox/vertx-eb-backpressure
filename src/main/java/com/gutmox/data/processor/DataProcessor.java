5fd75f35c96f5e57ad5a3a35fd596aead2f377c4
Pablo Gutierrez <gutmox@gmail.com>
2020-09-08 11:13:24 +0200

    Displaying errors

--- src/main/java/com/gutmox/data/processor/DataProcessor.java
+++ src/main/java/com/gutmox/data/processor/DataProcessor.java
@@ -8,13 +8,10 @@ import io.vertx.reactivex.core.Vertx;
 public class DataProcessor {
 
 	public DataProcessor(Vertx vertx, RedisDataDao redisDataDao) {
-		vertx.eventBus().consumer("data").setMaxBufferedMessages(300).toFlowable()
-			.flatMapCompletable(msg -> {
-				System.out.println(msg.body());
-				return redisDataDao.getKey(msg.body().toString()).flatMapCompletable(rs -> {
-					msg.reply(new JsonObject().put("name", rs));
-					return Completable.complete();
-				});
-			}).subscribe();
+		vertx.eventBus().consumer("data").setMaxBufferedMessages(1).toFlowable()
+			.flatMapCompletable(msg -> redisDataDao.getKey(msg.body().toString()).flatMapCompletable(rs -> {
+				msg.reply(new JsonObject().put("name", rs));
+				return Completable.complete();
+			})).doOnError(Throwable::printStackTrace).subscribe();
 	}
 }
